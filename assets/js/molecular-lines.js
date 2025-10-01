593105c2153de2175fa253eae1162e373d395b80
Copland-yz <coplandyong@gmail.com>
2025-10-01 17:39:54 -0500

    All about Deuterium - Added page 369 to 377 of molecules with D - Updated html and js relevant to D

--- assets/js/molecular-lines.js
+++ assets/js/molecular-lines.js
@@ -6,12 +6,12 @@ const elements = [
     ['K', 19, 'Potassium'], ['Ca', 20, 'Calcium'], ['Sc', 21, 'Scandium'], ['Ti', 22, 'Titanium'], ['V', 23, 'Vanadium'], ['Cr', 24, 'Chromium'], ['Mn', 25, 'Manganese'], ['Fe', 26, 'Iron'], ['Co', 27, 'Cobalt'], ['Ni', 28, 'Nickel'], ['Cu', 29, 'Copper'], ['Zn', 30, 'Zinc'], ['Ga', 31, 'Gallium'], ['Ge', 32, 'Germanium'], ['As', 33, 'Arsenic'], ['Se', 34, 'Selenium'], ['Br', 35, 'Bromine'], ['Kr', 36, 'Krypton'],
     ['Rb', 37, 'Rubidium'], ['Sr', 38, 'Strontium'], ['Y', 39, 'Yttrium'], ['Zr', 40, 'Zirconium'], ['Nb', 41, 'Niobium'], ['Mo', 42, 'Molybdenum'], ['Tc', 43, 'Technetium'], ['Ru', 44, 'Ruthenium'], ['Rh', 45, 'Rhodium'], ['Pd', 46, 'Palladium'], ['Ag', 47, 'Silver'], ['Cd', 48, 'Cadmium'], ['In', 49, 'Indium'], ['Sn', 50, 'Tin'], ['Sb', 51, 'Antimony'], ['Te', 52, 'Tellurium'], ['I', 53, 'Iodine'], ['Xe', 54, 'Xenon'],
     ['Cs', 55, 'Cesium'], ['Ba', 56, 'Barium'], ['La', 57, 'Lanthanum'], ['Ce', 58, 'Cerium'], ['Pr', 59, 'Praseodymium'], ['Nd', 60, 'Neodymium'], ['Pm', 61, 'Promethium'], ['Sm', 62, 'Samarium'], ['Eu', 63, 'Europium'], ['Gd', 64, 'Gadolinium'], ['Tb', 65, 'Terbium'], ['Dy', 66, 'Dysprosium'], ['Ho', 67, 'Holmium'], ['Er', 68, 'Erbium'], ['Tm', 69, 'Thulium'], ['Yb', 70, 'Ytterbium'], ['Lu', 71, 'Lutetium'], ['Hf', 72, 'Hafnium'], ['Ta', 73, 'Tantalum'], ['W', 74, 'Tungsten'], ['Re', 75, 'Rhenium'], ['Os', 76, 'Osmium'], ['Ir', 77, 'Iridium'], ['Pt', 78, 'Platinum'], ['Au', 79, 'Gold'], ['Hg', 80, 'Mercury'], ['Tl', 81, 'Thallium'], ['Pb', 82, 'Lead'], ['Bi', 83, 'Bismuth'], ['Po', 84, 'Polonium'], ['At', 85, 'Astatine'], ['Rn', 86, 'Radon'],
-    ['Fr', 87, 'Francium'], ['Ra', 88, 'Radium'], ['Ac', 89, 'Actinium'], ['Th', 90, 'Thorium'], ['Pa', 91, 'Protactinium'], ['U', 92, 'Uranium'], ['Np', 93, 'Neptunium'], ['Pu', 94, 'Plutonium'], ['Am', 95, 'Americium'], ['Cm', 96, 'Curium'], ['Bk', 97, 'Berkelium'], ['Cf', 98, 'Californium'], ['Es', 99, 'Einsteinium'], ['Fm', 100, 'Fermium'], ['Md', 101, 'Mendelevium'], ['No', 102, 'Nobelium'], ['Lr', 103, 'Lawrencium'], ['Rf', 104, 'Rutherfordium'], ['Db', 105, 'Dubnium'], ['Sg', 106, 'Seaborgium'], ['Bh', 107, 'Bohrium'], ['Hs', 108, 'Hassium'], ['Mt', 109, 'Meitnerium'], ['Ds', 110, 'Darmstadtium'], ['Rg', 111, 'Roentgenium'], ['Cn', 112, 'Copernicium'], ['Nh', 113, 'Nihonium'], ['Fl', 114, 'Flerovium'], ['Mc', 115, 'Moscovium'], ['Lv', 116, 'Livermorium'], ['Ts', 117, 'Tennessine'], ['Og', 118, 'Oganesson']
+    ['Fr', 87, 'Francium'], ['Ra', 88, 'Radium'], ['Ac', 89, 'Actinium'], ['Th', 90, 'Thorium'], ['Pa', 91, 'Protactinium'], ['U', 92, 'Uranium'], ['Np', 93, 'Neptunium'], ['Pu', 94, 'Plutonium'], ['Am', 95, 'Americium'], ['Cm', 96, 'Curium'], ['Bk', 97, 'Berkelium'], ['Cf', 98, 'Californium'], ['Es', 99, 'Einsteinium'], ['Fm', 100, 'Fermium'], ['Md', 101, 'Mendelevium'], ['No', 102, 'Nobelium'], ['Lr', 103, 'Lawrencium'], ['Rf', 104, 'Rutherfordium'], ['Db', 105, 'Dubnium'], ['Sg', 106, 'Seaborgium'], ['Bh', 107, 'Bohrium'], ['Hs', 108, 'Hassium'], ['Mt', 109, 'Meitnerium'], ['Ds', 110, 'Darmstadtium'], ['Rg', 111, 'Roentgenium'], ['Cn', 112, 'Copernicium'], ['Nh', 113, 'Nihonium'], ['Fl', 114, 'Flerovium'], ['Mc', 115, 'Moscovium'], ['Lv', 116, 'Livermorium'], ['Ts', 117, 'Tennessine'], ['Og', 118, 'Oganesson'], ['D', 119, 'Deuterium']
 ];
 
 // Periodic table layout (positions for each element)
 const layout = [
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+    [1, 119, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
     [3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 6, 7, 8, 9, 10],
     [11, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13, 14, 15, 16, 17, 18],
     [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
@@ -173,18 +173,28 @@ function generatePeriodicTable() {
                 table.appendChild(emptyCell);
             } else {
                 // Element cell
-                const element = elements[atomicNumber - 1];
+                let element, realAtomicNumber;
+                
+                if (atomicNumber === 119) {
+                    // Special case for Deuterium
+                    element = elements[elements.length - 1]; // Last element is D
+                    realAtomicNumber = 1; // D has atomic number 1
+                } else {
+                    element = elements[atomicNumber - 1];
+                    realAtomicNumber = atomicNumber;
+                }
+                
                 const elementDiv = document.createElement('div');
-                elementDiv.className = 'element';
-                elementDiv.setAttribute('data-atomic-number', atomicNumber);
+                elementDiv.className = atomicNumber === 119 ? 'element deuterium' : 'element';
+                elementDiv.setAttribute('data-atomic-number', realAtomicNumber);
                 elementDiv.title = element[2]; // Element name
                 
                 elementDiv.innerHTML = `
-                    <div class="number">${atomicNumber}</div>
+                    <div class="number">${realAtomicNumber}</div>
                     <div class="symbol">${element[0]}</div>
                 `;
                 
-                elementDiv.onclick = () => toggleElement(atomicNumber, elementDiv);
+                elementDiv.onclick = () => toggleElement(realAtomicNumber, elementDiv);
                 table.appendChild(elementDiv);
             }
         });
